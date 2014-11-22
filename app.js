ae17475d7644e6f59b0d873b50eaafb0d555c43c
Dammmien <d.damien@live.fr>
2014-11-22 13:23:01 +0100

    manifest

--- app.js
+++ app.js
@@ -1,34 +1,32 @@
 window.addEventListener( "load", function() {
 
-  console.log( 'ok' );
+    navigator.getUserMedia = ( navigator.getUserMedia ||
+        navigator.webkitGetUserMedia ||
+        navigator.mozGetUserMedia ||
+        navigator.msGetUserMedia );
 
-  navigator.getUserMedia = ( navigator.getUserMedia ||
-    navigator.webkitGetUserMedia ||
-    navigator.mozGetUserMedia ||
-    navigator.msGetUserMedia );
+    if ( navigator.getUserMedia ) {
 
-  if ( navigator.getUserMedia ) {
+        navigator.getUserMedia( {
+                video: true
+            },
 
-    navigator.getUserMedia( {
-        video: true
-      },
+            // successCallback
+            function( localMediaStream ) {
+                var video = document.querySelector( 'video' );
+                video.src = window.URL.createObjectURL( localMediaStream );
+                video.play()
+            },
 
-      // successCallback
-      function( localMediaStream ) {
-        var video = document.querySelector( 'video' );
-        video.src = window.URL.createObjectURL( localMediaStream );
-        video.play()
-      },
+            // errorCallback
+            function( err ) {
+                console.log( "The following error occured: " + err );
+            }
 
-      // errorCallback
-      function( err ) {
-        console.log( "The following error occured: " + err );
-      }
+        );
 
-    );
-
-  } else {
-    console.log( "getUserMedia not supported" );
-  }
+    } else {
+        console.log( "getUserMedia not supported" );
+    }
 
 } );
