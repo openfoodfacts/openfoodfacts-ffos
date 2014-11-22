fc4718bc08676514198eec0225962d3b89c93d49
Philippe Joulot <philippe.joulot@laposte.net>
2014-11-22 19:49:49 +0100

    Version using system apps

--- app.js
+++ app.js
@@ -1,47 +1,29 @@
 window.addEventListener( "load", function() {
+	document.querySelector('#pick-image').addEventListener ('click', function () {
 
-    var stream = null;
-    var video = document.querySelector( 'video' );
-    var canvas = document.querySelector( 'canvas' );
-    var context = canvas.getContext( '2d' );
-    var w, h, ratio;
+	var pick = new MozActivity({
+		name: "pick",
+		data: {
+		    type: "image/jpeg",
+		    // In FxOS 1.3 and before the user is allowed to crop the
+		    // image by default, but this can cause out-of-memory issues
+		    // so we explicitly disable it.
+		    nocrop: true // don't allow the user to crop the image
+		}
+	    });
+	console.log("test2");
 
-    navigator.getUserMedia = ( navigator.getUserMedia ||
-        navigator.webkitGetUserMedia ||
-        navigator.mozGetUserMedia ||
-        navigator.msGetUserMedia );
+	    pick.onsuccess = function () {
+		var img = document.createElement("img");
+		img.src = window.URL.createObjectURL(this.result.blob);
+		var imagePresenter = document.querySelector("#image-presenter");
+		imagePresenter.appendChild(img);
+		imagePresenter.style.display = "block";
+	    };
 
-    if ( navigator.getUserMedia ) {
-
-        navigator.getUserMedia( {
-                video: true
-            },
-
-            function( localMediaStream ) {
-                video.src = window.URL.createObjectURL( localMediaStream );
-                video.play();
-            },
-
-            function( err ) {
-                console.log( "The following error occured: " + err );
-            }
-
-        );
-
-    } else {
-        console.log( "getUserMedia not supported" );
-    }
-
-    takeScreenshot = function() {
-        ratio = video.videoWidth / video.videoHeight;
-        w = video.videoWidth - 100;
-        h = parseInt( w / ratio, 10 );
-        canvas.width = w;
-        canvas.height = h;
-        context.fillRect( 0, 0, w, h );
-        context.drawImage( video, 0, 0, w, h );
-    };
-
-    document.addEventListener( 'touchstart', takeScreenshot );
+	    pick.onerror = function () {
+		console.log("Can't view the image");
+	    };
+	});
 
 } );
