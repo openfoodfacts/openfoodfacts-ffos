5adc4dd49b3c97b76387f829e317ce57d1d261b7
Philippe Joulot <philippe.joulot@laposte.net>
2014-11-23 14:30:14 +0100

    Put blob in a canvas

--- app.js
+++ app.js
@@ -1,47 +1,54 @@
 window.addEventListener( "load", function() {
 
-    var stream = null;
-    var video = document.querySelector( 'video' );
-    var canvas = document.querySelector( 'canvas' );
-    var context = canvas.getContext( '2d' );
-    var w, h, ratio;
+	 var canvas = document.querySelector( 'canvas' );
+    	 var context = canvas.getContext( '2d' );
+    	 var w, h, ratio;
 
-    navigator.getUserMedia = ( navigator.getUserMedia ||
-        navigator.webkitGetUserMedia ||
-        navigator.mozGetUserMedia ||
-        navigator.msGetUserMedia );
+	function draw(urlImage) {
+	  var ctx = document.getElementById('canvas').getContext('2d');
+	  var img = new Image();
+	  img.src = urlImage;
+	  img.onload = function(){
+	    canvas.width = img.width;
+	    canvas.height = img.height;
+	    ctx.drawImage(img,0,0);
+	  }
+	}
 
-    if ( navigator.getUserMedia ) {
 
-        navigator.getUserMedia( {
-                video: true
-            },
+	document.querySelector('#pick-image').addEventListener ('click', function () {
 
-            function( localMediaStream ) {
-                video.src = window.URL.createObjectURL( localMediaStream );
-                video.play();
-            },
+	var pick = new MozActivity({
+		name: "pick",
+		data: {
+		    type: "image/jpeg",
+		    // In FxOS 1.3 and before the user is allowed to crop the
+		    // image by default, but this can cause out-of-memory issues
+		    // so we explicitly disable it.
+		    nocrop: true // don't allow the user to crop the image
+		}
+	    });
 
-            function( err ) {
-                console.log( "The following error occured: " + err );
-            }
+	    pick.onsuccess = function () {
+		
+		draw(window.URL.createObjectURL(this.result.blob));
+		
+		dataURL = canvas.toDataURL();
+		
+		console.log(dataURL);
+		var reader = new FileReader();
+		reader.onloadend = function () {
+		  //reader.result image en base 64
+	          console.log(reader.result);
+	        }
+	        
+		reader.readAsDataURL(this.result.blob);
+		
+	    };
 
-        );
-
-    } else {
-        console.log( "getUserMedia not supported" );
-    }
-
-    takeScreenshot = function() {
-        ratio = video.videoWidth / video.videoHeight;
-        w = video.videoWidth - 100;
-        h = parseInt( w / ratio, 10 );
-        canvas.width = w;
-        canvas.height = h;
-        context.fillRect( 0, 0, w, h );
-        context.drawImage( video, 0, 0, w, h );
-    };
-
-    document.addEventListener( 'touchstart', takeScreenshot );
+	    pick.onerror = function () {
+		console.log("Can't view the image");
+	    };
+	});
 
 } );
